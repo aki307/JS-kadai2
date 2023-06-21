359fc464e6f160f86576b742dc082686a1be6820
Ubuntu <ubuntu@ip-172-31-28-133.ap-northeast-1.compute.internal>
2023-06-21 11:05:35 +0000

    修正版

--- main.js
+++ main.js
@@ -4,63 +4,19 @@ let button = document.getElementById('button-execution');
 let textbox_element = document.getElementById('textbox');
 let newElement = document.createElement('div');
 let displayNumber = '';
-let fizzNumber = [];
-let buzzNumber = [];
-let fizzBuzzNumber = [];
-let calNumber = [];
-let loopCount = 0;
-
-function GCD(A, B) {
-  if (B === 0) return A;
-  return GCD(B, A % B);
-}
-
-function campareFunc(a, b) {
-  return a - b;
-}
 
 function calNum(fizzValue, buzzValue) {
-  let fizzLoopCount = Math.floor(99 / fizzValue);
-  let buzzLoopCount = Math.floor(99 / buzzValue);
-  if (fizzLoopCount <= buzzLoopCount) {
-    loopCount = buzzLoopCount;
-  }
-  else {
-    loopCount = fizzLoopCount;
-  }
-
-  let leastCommonMultiple = Math.floor(fizzValue / GCD(fizzValue, buzzValue)) * buzzValue;
-
-  let fizzBuzzLoopCount = Math.floor(99 / leastCommonMultiple);
-  for (let i = 1; i < fizzBuzzLoopCount; i++) {
-    fizzBuzzNumber[i] = i * leastCommonMultiple;
-  }
-  for (let i = 1; i <= fizzLoopCount; i++) {
-    fizzNumber[i] = i * fizzValue;
-  }
-  for (let i = 1; i <= buzzLoopCount; i++) {
-    buzzNumber[i] = i * buzzValue;
-  }
-  for (let i = 0; i <= loopCount; i++) {
-    let fizzIndex = 2 * i - 2;
-    let buzzIndex = 2 * i - 1;
-    calNumber[fizzIndex] = fizzNumber[i];
-    calNumber[buzzIndex] = buzzNumber[i];
-  }
-  calNumber.sort(campareFunc);
-  let set = new Set(calNumber);
-  let setNumber = Array.from(set);
-  setNumber.forEach((number) => {
-    if (number % leastCommonMultiple == 0) {
-      displayNumber += `<p>fizzBuzz:${number}</p>`;
+  for (let i = 1; i < 100; i++) {
+    if (i % fizzValue === 0 && i % buzzValue === 0) {
+      displayNumber += `<p>fizzBuzz:${i}</p>`;
     }
-    else if (number % fizzValue == 0) {
-      displayNumber += `<p>fizz:${number}</p>`;
+    else if (i % fizzValue === 0) {
+      displayNumber += `<p>fizz:${i}</p>`;
     }
-    else if (number % buzzValue == 0) {
-      displayNumber += `<p>buzz:${number}</p>`;
+    else if (i % buzzValue === 0) {
+      displayNumber += `<p>buzz:${i}</p>`;
     }
-  });
+  }
   textbox_element.insertAdjacentHTML('afterbegin', displayNumber);
 }
 button.addEventListener('click', () => {
